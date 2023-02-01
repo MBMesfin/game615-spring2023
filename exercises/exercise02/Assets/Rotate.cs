d85ca709413c4a30b7e86537922baff1a3e6b81d
MBMesfin <120344206+MBMesfin@users.noreply.github.com>
2023-02-01 16:20:33 -0500

    Update

---
+++ exercises/exercise02/Assets/Rotate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(gameObject.name);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    
+       // gameObject.transform.Rotate(0,1f,0);
+        // gameObject.transform.rotation.y = gameObject.transform.rotation.y + 1; 
+    }
+}
